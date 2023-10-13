9a60fe3332e2c1cf1b5772d5a319072fc10fc980
shecannotsee <shecannotsee@163.com>
2023-10-13 18:18:33 +0800

    [feat] add framework

---
+++ cmake/she_interpreter-config.cmake
@@ -0,0 +1,13 @@
+
+# ${she_interpreter_FOUND}       : right get
+# ${she_interpreter_INCLUDE_DIR} : include
+# ${she_interpreter_LIBRARIES}   : lib
+# ${she_interpreter_LINK_TARGET} : link target
+
+set(she_interpreter "she_interpreter")
+set(${she_interpreter}_FOUND "1")
+set(${she_interpreter}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${she_interpreter}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${she_interpreter}_LINK_TARGET   "she_interpreter")
+
+
