39f5b93afbedd57c1e5120ad689782651db3bdc5
shecannotsee <shecannotsee@163.com>
2023-10-19 15:08:45 +0800

    [feat] add framework

---
+++ src/IR/static_analysis.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#ifndef SHE_INTERPRETER_STATIC_ANALYSIS_H
+#define SHE_INTERPRETER_STATIC_ANALYSIS_H
+
+namespace IR {
+
+class static_analysis {
+
+};
+
+} // IR
+
+#endif //SHE_INTERPRETER_STATIC_ANALYSIS_H
