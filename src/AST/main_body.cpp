575aaa313675d703bbba10d87b98d335bcac62ed
shecannotsee <shecannotsee@163.com>
2023-10-19 15:05:53 +0800

    [feat] add framework

---
+++ src/AST/main_body.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#include "main_body.h"
+
+namespace AST {
+} // AST
---
+++ src/AST/main_body.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#ifndef SHE_INTERPRETER_MAIN_BODY_H
+#define SHE_INTERPRETER_MAIN_BODY_H
+
+namespace AST {
+
+class main_body {
+
+};
+
+} // AST
+
+#endif //SHE_INTERPRETER_MAIN_BODY_H
