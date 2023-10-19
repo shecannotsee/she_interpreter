ebcfa8e65cbe8534902ff58b1805c4ae99213fd8
shecannotsee <shecannotsee@163.com>
2023-10-19 14:32:59 +0800

    [feat] add framework

---
+++ src/stp1_lexical_analysis/process_note.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#include "process_note.h"
+
+namespace lexical_analysis {
+} // lexical_analysis
---
+++ src/stp1_lexical_analysis/process_note.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#ifndef SHE_INTERPRETER_PROCESS_NOTE_H
+#define SHE_INTERPRETER_PROCESS_NOTE_H
+
+namespace lexical_analysis {
+
+class process_note {
+
+};
+
+} // lexical_analysis
+
+#endif //SHE_INTERPRETER_PROCESS_NOTE_H
