55ba6872a61e9b73cf1a6d4cff2be7cd00e27e6f
shecannotsee <shecannotsee@163.com>
2023-10-19 14:46:05 +0800

    [feat] add syntax_type

---
+++ src/stp2_syntax_analysis/syntax_type.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#ifndef SHE_INTERPRETER_SYNTAX_TYPE_H
+#define SHE_INTERPRETER_SYNTAX_TYPE_H
+
+namespace syntax_analysis {
+
+enum class syntax_type : int {
+  
+};
+
+}// namespace syntax_analysis
+
+#endif //SHE_INTERPRETER_SYNTAX_TYPE_H
