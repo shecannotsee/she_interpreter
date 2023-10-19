5e9face17ca95ad9ba047722100bab5a118cf6ab
shecannotsee <shecannotsee@163.com>
2023-10-19 15:18:56 +0800

    [feat] add valid_char tuple

---
+++ src/valid_char.h
@@ -0,0 +1,25 @@
+//
+// Created by shecannotsee on 23-10-19.
+//
+
+#ifndef SHE_INTERPRETER_VALID_CHAR_H
+#define SHE_INTERPRETER_VALID_CHAR_H
+
+#include <tuple>
+#include <vector>
+
+struct valid_char {
+  valid_char() = default;
+  std::vector<std::tuple<int,int>> index;
+  int tuples() const {
+    return this->index.size();
+  };
+  static int get_start(std::vector<std::tuple<int,int>> _,int _index){
+    return std::get<0>(_[_index]);
+  }
+  static int get_end(std::vector<std::tuple<int,int>> _,int _index){
+    return std::get<1>(_[_index]);
+  }
+};
+
+#endif //SHE_INTERPRETER_VALID_CHAR_H
